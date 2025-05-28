a50089ae77e6152cd0bb0ef08f4ed1b829f0456c
Mahir Piyarali <mahirpiy@umich.edu>
2025-05-27 21:39:03 -0700

    fix db issues

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,9 +1,5 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
-import * as schema from "./schema";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-export const db = drizzle(pool, { schema });
+const client = postgres(process.env.DATABASE_URL!);
+export const db = drizzle({ client });
